8b827bcda5347edeed67c3ee73d7f06c27e7bced
Mohamed Ayadi <Jouba@Mohameds-MacBook-Air.local>
2016-05-31 14:28:00 -0500

    First commit

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property NSMutableArray *titles;
+@property NSMutableArray *descriptions;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -16,12 +19,92 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.titles = [NSMutableArray new];
+
+    
+}
+- (IBAction)edit:(UIBarButtonItem *)sender
+{
+    if (self.editing)
+    {
+        self.editing = false;
+        [self.tableView setEditing:false animated:true];
+        sender.style = UIBarButtonItemStylePlain;
+        sender.title = @"Edit";
+    }
+    else
+    {
+        self.editing = true;
+        [self.tableView setEditing:true animated:true];
+        sender.style =  UIBarButtonItemStyleDone;
+        sender.title = @"Done";
+        
+        
+    }
+    [self.tableView reloadData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    
+    NSString *title =[self.titles objectAtIndex:sourceIndexPath.row];
+    [self.titles removeObject:title];
+    [self.titles insertObject:title atIndex:destinationIndexPath.row];
+    
+    NSString *description =[self.descriptions objectAtIndex:sourceIndexPath.row];
+    [self.descriptions removeObject:description];
+    [self.descriptions insertObject:description atIndex:destinationIndexPath.row];
+    
 }
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return true;
+    
+}
+- (IBAction)add:(UIBarButtonItem *)sender {
+    [self presentDreamEntry];
+}
+
+
+-(void) presentDreamEntry{
+    UIAlertController *alertContoller = [UIAlertController alertControllerWithTitle:@"Enter text" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    [alertContoller addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"Text";
+    }];
+    
+    
+    UIAlertAction *cancelAction =[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    UIAlertAction *saveAction =[UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction *_Nonnull action){
+        UITextField *textField1 = alertContoller.textFields.firstObject;
+        
+        
+        [self.titles addObject:textField1.text];
+        [self.tableView reloadData];
+        
+    }];
+    [alertContoller addAction:cancelAction];
+    [alertContoller addAction:saveAction];
+    
+    [self presentViewController:alertContoller animated:true completion:nil];
+    
+    
+    
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"CellID"];
+    cell.textLabel.text = [self.titles objectAtIndex:indexPath.row];
+    cell.detailTextLabel.text = [self.descriptions objectAtIndex:indexPath.row];
+    return  cell;
+}
+
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    
+    return self.titles.count;
+    
+}
+
 
 @end
